80c29e0d4c4199177cdb1495a56da416c49605ee
James Dickinson <james.dickinson@willistowerswatson.com>
2022-04-12 12:04:43 +0100

    Adding dto for holding Shakespearean Pokemon description.

---
+++ TrueLayerChallenge/TrueLayerChallenge.WebApi/Dtos/ShakespeareanPokemonDescriptionDto.cs
@@ -0,0 +1,29 @@
+﻿namespace TrueLayerChallenge.WebApi.Dtos
+{
+    /// <summary>
+    /// Data transfer object containing the Shakespearean description of a given pokemon.
+    /// </summary>
+    public class ShakespeareanPokemonDescriptionDto
+    {
+        /// <summary>
+        /// Creates a new <see cref="ShakespeareanPokemonDescriptionDto"/>.
+        /// </summary>
+        /// <param name="name">Name of the pokemon for which the <see cref="ShakespeareanPokemonDescriptionDto"/> corresponds to.</param>
+        /// <param name="description">Shakespearean description of the pokemon with <paramref name="name"/>.</param>
+        public ShakespeareanPokemonDescriptionDto(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        /// <summary>
+        /// Name of the pokemon for which the <see cref="ShakespeareanPokemonDescriptionDto"/> corresponds to.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The Shakespearean description of the pokemon.
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
