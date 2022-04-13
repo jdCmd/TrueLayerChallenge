9a5cf81ffc259b935574da161a89fc61b1b074b4
James Dickinson <james.dickinson@willistowerswatson.com>
2022-04-13 14:50:43 +0100

    Adding enum for translation types

---
+++ TrueLayerChallenge/TrueLayerChallenge.WebApi/Enums/FunTranslation.cs
@@ -0,0 +1,14 @@
+namespace TrueLayerChallenge.WebApi.Enums;
+
+/// <summary>
+/// Enum to represent the available Fun Translation translations.
+/// </summary>
+internal enum FunTranslation
+{
+    /// <summary>
+    /// Enum value representing the Fun Translations Shakespeare translation.
+    /// </summary>
+    Shakespeare
+
+    // Could add more at a later date etc...
+}
